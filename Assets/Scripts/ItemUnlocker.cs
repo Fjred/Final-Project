688f33c4a12b96acc3eac40f0b71b8296cc88649
Fjred <karolis.aukstakojis@gmail.com>
2023-05-28 16:56:34 +0300

    2 new locks and AI walking and chasing

---
+++ Assets/Scripts/ItemUnlocker.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemUnlocker : MonoBehaviour
+{
+    public GameObject obstacle;
+
+    public PickAndDropController item;
+
+    public bool unlocked = false;
+
+    public GameObject hiddenObject;
+    public GameObject visibleObject;
+
+    [SerializeField] private Material buttonColor;
+
+    private void Start()
+    {
+        buttonColor.color = Color.red;
+    }
+    void Unlock()
+    {
+        if (item.isPickedUp != true) return;
+
+        obstacle.GetComponent<Rigidbody>().isKinematic = false;
+
+        unlocked = true;
+    }
+
+    void CutOff()
+    {
+        if (item.isPickedUp != true) return;
+
+        unlocked = true;
+
+        buttonColor.color = Color.green;
+
+        visibleObject.active = false;
+        hiddenObject.active = true;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (Input.GetKey(KeyCode.F))
+            {
+                if(item.gameObject.tag != "Pincers") Unlock();
+
+                CutOff();
+            }
+        }
+    }
+}
